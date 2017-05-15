597bba957c06b21363ef52194761f305e7256b02
Diurno <Diurno@C8PC14>
2017-05-15 11:44:46 +0200

    Segundo comit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ public class Main {
 
 	public Main() {
 		// TODO Auto-generated constructor stub
-		System.out.println("Hola mundo");		
+		System.out.println("Hola mundo");	
+		System.out.println("Hola mundo");
 	}
 
 }
